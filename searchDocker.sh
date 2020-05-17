968aa309afe2c45dc3be651946ad0f4c8b4d9bdb
byron7 <byron7@users.noreply.github.com>
2020-05-17 16:17:33 -0500

    Update searchDocker.sh

--- searchDocker.sh
+++ searchDocker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo $1
-fileDocker="find / -name '${1}'* > /home/osboxes/Documents/archivosDocker"
-buscar="find / -name *'${1}'*"
+fileDocker="find / -name '${1}'* > ${pwd}/archivosDocker"
+buscar="find / -name ${1}"
 archivos=$(eval "$buscar")
 eval "$fileDocker"
 echo "$archivos"
@@ -10,17 +10,17 @@ echo "$archivos"
 for n in ${archivos}
 do
 	if [[ -d $n ]]; then
-		dirtype="echo '$n is a directory' >> /home/osboxes/Documents/typeOfFiles"
+		dirtype="echo '$n is a directory' >> ${pwd}/typeOfFiles"
 		eval "$dirtype"
 		echo "$n is a directory"
-		subDirectorios=$(eval "ls -ltR $n >> /home/osboxes/Documents/archivosDocker")
+		subDirectorios=$(eval "ls -ltR $n >> ${pwd}/archivosDocker")
 		tree=$(eval "tree $n >> ${n//\//.}")
 	elif [[ -f $n ]]; then
-                filetype="echo '$n is a file' >> /home/osboxes/Documents/typeOfFiles"
+                filetype="echo '$n is a file' >> ${pwd}/typeOfFiles"
 		eval "$filetype"
 		echo "$n is a file"
 	else
-                othertype="echo '$n is another type' >> /home/osboxes/Documents/typeOfFiles"
+                othertype="echo '$n is another type' >> ${pwd}/typeOfFiles"
 		eval "$othertype"
 		echo "$n is not valid"
 #		exit 1
